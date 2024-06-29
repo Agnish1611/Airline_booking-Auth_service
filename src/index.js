14f9a6067e47dd9bd71948059ecaa705d3b20d14
Agnish1611 <sukla.baruipur2@gmail.com>
2024-06-30 02:44:08 +0530

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server-config');
+
+// const apiRoutes = require('./routes');
+
+const setupServer = async () => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    // app.use('/api', apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server started at PORT ${PORT}`);
+    });
+}
+
+setupServer();
